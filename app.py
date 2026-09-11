cf9f764e34c67be705b3cc4ab16a67e5986da2e9
Ramazan <kholmatov1988@bk.ru>
2026-09-11 17:28:47 +0500

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+st.markdown("""
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+""", unsafe_allow_html=True)
 import streamlit as st
 import qrcode
 from PIL import Image
